f5db14495d3efc537d213997e86978af9f135d87
Pavani369 <83334393+Pavani369@users.noreply.github.com>
2021-05-18 19:20:35 +0530

    Add files via upload

---
+++ s.js
@@ -0,0 +1,23 @@
+const { response } = require("express");
+const express= require("express");
+const bodyParser=require("body-parser");
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.get("/",function(req,res)
+{
+    res.sendFile(__dirname+"/in.html");
+});
+
+app.post("/",function(req,res)
+{    
+    var n1=Number(req.body.num1);
+    var n2=Number(req.body.num2);
+    var result=n1+n2;
+     res.send("This is answer " + result);   
+});
+
+
+app.listen(3400,function()
+{
+    console.log("Server started on 3400 port");
+});
